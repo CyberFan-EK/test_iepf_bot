4ddecf92f52dc3678eef6f9b6fac4e265a04a85a
CyberFan-EK <merginskiy@gmail.com>
2022-12-27 23:32:41 +0200

    first real work

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,17 @@
 import TeleBot from "telebot";
+const { Telegraf, Markup, Extra, Stage, session } = require("telegraf");
+const token = process.env.BOT_TOKEN;
+const fs = require("fs");
+const filePath = "messages/message.txt";
+if (token === undefined) {
+  throw new Error("BOT_TOKEN must be provided!");
+}
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+// const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(token, { polling: true });
 
-bot.on("text", (msg) => msg.reply.text("Hello" + msg.text));
+const startBot = require("./src/commands/start");
+startBot(bot);
+// bot.on("text", (msg) => msg.reply.text("Hello" + msg.text));
 
 export default bot;
