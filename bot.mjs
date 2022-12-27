dee544b02ebc6a6be37c40355aa3356c6e9820fa
CyberFan-EK <merginskiy@gmail.com>
2022-12-27 23:50:37 +0200

    first4 real work

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,7 @@
 import TeleBot from "telebot";
 const { Telegraf, Markup, Extra, Stage, session } = require("telegraf");
 const token = process.env.BOT_TOKEN;
-const fs = require("fs");
+// const fs = require("fs");
 const filePath = "messages/message.txt";
 if (token === undefined) {
   throw new Error("BOT_TOKEN must be provided!");
