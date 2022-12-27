2988b248ab4b5a60a1d925365aa85a026ff3c6c0
CyberFan-EK <merginskiy@gmail.com>
2022-12-27 23:26:01 +0200

    second test

--- bot.mjs
+++ bot.mjs
@@ -2,6 +2,6 @@ import TeleBot from "telebot";
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => msg.reply.text("Hello world" + msg.text));
+bot.on("text", (msg) => msg.reply.text("Hello" + msg.text));
 
 export default bot;
